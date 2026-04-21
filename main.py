744d56ed6c17118c5ce25447fdba1ee64f4a4b48
John Pawlowski <john.pawlowski93@gmail.com>
2026-04-21 12:59:46 -0700

    scaffold main CLI structure

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from bank import BankAccount
 
 def main():
+    #start with empty accounts list
+
+    #print the list of options
+    ##-----Bank CLI-----
+    ## 1. Create new account
+    ## 2. Make a deposit
+    ## 3. Make a withdrawal
+    ## 4. Make a transfer
+    ## 5. View account transaction history
+    ## 6. List accounts
+    ## 7. Exit
+
+    #capture the user choice
+
+    #conditional logic
+    ##if 1
+    ##elif (2,3,4,5)
+    ##elif 6
+    ##else:
+    ##    exit()
     return
 
 if __name__ == "__main":
