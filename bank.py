d7c67131244e9e354347e50335a50e13fc7dbdcf
John Pawlowski <john.pawlowski93@gmail.com>
2026-04-21 12:16:11 -0700

    update __init__ in BankAccount

--- bank.py
+++ bank.py
@@ -1,4 +1,6 @@
 class BankAccount:
     
-    def __init__(self):
-        pass
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+        self.transactions = []
